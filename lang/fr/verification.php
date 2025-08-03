6aecb444245c75b2e5f952204b6fe157643c606b
fhemery <f.hemery@hemit.fr>
2025-08-03 21:18:48 +0200

    feat(user): manage translations for user content

---
+++ lang/fr/verification.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'verified' => 'Votre adresse email a été vérifiée avec succès.',
+    'already_verified' => 'Votre adresse email est déjà vérifiée.',
+    'user' => 'Aucun utilisateur n\'a été trouvé avec cette adresse email.',
+    'sent' => 'Nous vous avons envoyé un email de vérification !',
+    'throttled' => 'Veuillez patienter avant de réessayer.',
+];
