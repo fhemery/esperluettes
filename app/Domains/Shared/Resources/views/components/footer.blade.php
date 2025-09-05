f6a4f2e21ad3fbe11a33c34aeeef87aa6f7ac847
fhemery <f.hemery@hemit.fr>
2025-09-05 16:01:44 +0200

    fix: remove & signs from footer
    They were breaking the design on half screens, and were too small as is.

--- app/Domains/Shared/Resources/views/components/footer.blade.php
+++ app/Domains/Shared/Resources/views/components/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="min-h-4 bg-primary w-full"></div>
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
         <img src="{{ asset('images/themes/autumn/logo-full.png') }}" alt="{{config('app.name')}}" class="h-24">
-        <div class="grid grid-cols-3 md:grid-cols-6 gap-8 md:-mt-4">
+        <div class="grid grid-cols-3 md:grid-cols-5 gap-8 md:-mt-4">
            <div class="col-span-3 md:col-span-2 md:pl-24">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus id nam ratione quibusdam officia alias, sint ipsa, nesciunt commodi minus veniam 
            </div>
@@ -33,11 +33,6 @@
                     <li><a href="#" class="hover:text-primary">CGU</a></li>
                     <li><a href="#" class="hover:text-primary">L’association</a></li>
                 </ul>
-            </div>
-            <!-- Big ampersand / decorative area -->
-            <div class="items-end hidden md:flex">
-                <div class="text-[96px] leading-none font-extrabold text-primary/10 select-none">&amp;</div>
-                <div class="text-[164px] leading-none font-extrabold text-primary/10 select-none">&amp;</div>
             </div>
         </div>
 
