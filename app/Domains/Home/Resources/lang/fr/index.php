800fc04cb55d803174c9faa3d4f5bb94e78511aa
fhemery <f.hemery@hemit.fr>
2025-09-10 07:23:16 +0200

    fix(home): incorporate translation for welcome message
    And shorten it

--- app/Domains/Home/Resources/lang/fr/index.php
+++ app/Domains/Home/Resources/lang/fr/index.php
@@ -1,5 +1,13 @@
 <?php
 
 return [
-    'join-us' => 'Rejoignez-nous',
+    'join-us' => 'Rejoins-nous',
+     'welcome-message' => '<p>Salut à toi qui arrives en ces lieux.</p>
+
+     <p>Voici l’entrée de notre havre : un jardin luxuriant où poussent les tiges, les feuilles, les vrilles de notre imagination.</p>
+
+     <p>Ici on s’entraide. On se lit, on se commente, on se conseille. Avec sincérité et bienveillance. Parce qu’on sait qu’écrire réclame du temps, du courage, de l’esprit et de l’âme. </p>
+
+     <p>Si ces quelques mots résonnent pour toi, scribouilleur fortuit ou autrice ambitieuse, lectrice picoreuse ou dévoreur d’histoires, alors pousse la barrière, viens poser tes cahiers.
+         Le Jardin des Esperluettes t’ouvre ses branches.</p>'
 ];
--- app/Domains/Home/Views/index.blade.php
+++ app/Domains/Home/Views/index.blade.php
@@ -7,18 +7,9 @@
                 <img src="{{ asset('images/themes/default/logo-white.png') }}" alt="{{config('app.name')}}" class="h-32">
             </div>
 
-            <div class="mt-12 space-y-4 leading-relaxed text-center text-white">
-                <p>Salut à toi qui arrives en ces lieux.</p>
+            <div class="mt-6 space-y-4 leading-relaxed text-center text-white"> {!! __('home::index.welcome-message') !!}</div>
 
-                <p>Ici se trouve l’entrée de notre havre : un jardin luxuriant où poussent les tiges, les feuilles, les vrilles de notre imagination. Les clairières sont peuplées de carnets, de plumes et de chapitres. Les tasses de thé sont toujours fumantes et les chats surgissent de nulle part, porteurs de mystères ou de réconfort. Parfois on croise une fée, un détective ou un vaisseau…</p>
-
-                <p>Ici on s’entraide. On se lit, on se commente, on se conseille. Avec sincérité et bienveillance. Parce qu’on sait qu’écrire réclame du temps, du courage, de l’esprit et de l’âme. </p>
-
-                <p>Si ces quelques mots résonnent pour toi, scribouilleur fortuit ou autrice ambitieuse, lectrice picoreuse ou dévoreur d’histoires, alors pousse la barrière, viens poser tes cahiers.
-                    Le Jardin des Esperluettes t’ouvre ses branches.</p>
-            </div>
-
-            <div class="mt-12 flex items-center justify-center">
+            <div class="mt-6 flex items-center justify-center">
                 <a href="{{ route('register') }}" class="btn-accent">{{ __('home::index.join-us') }}</a>
             </div>
         </div>
