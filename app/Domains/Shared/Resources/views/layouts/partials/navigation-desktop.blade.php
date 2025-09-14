351f38d76507ad566c8bc56249a329ea50963707
fhemery <f.hemery@hemit.fr>
2025-09-14 22:45:14 +0200

    fix(shared): remove display name from top bar

--- app/Domains/Shared/Resources/views/layouts/partials/navigation-desktop.blade.php
+++ app/Domains/Shared/Resources/views/layouts/partials/navigation-desktop.blade.php
@@ -27,14 +27,8 @@
             class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 hover:text-gray-700 focus:outline-none transition ease-in-out duration-150"
         >
             @if(isset($currentProfile) && $currentProfile)
-            <x-shared::avatar :src="$currentProfile->avatar_url" alt="avatar" class="h-6 w-6 rounded-full me-2" />
-            <div>{{ $currentProfile->display_name }}</div>
-            @else
-            <div>{{ Auth::user()->email }}</div>
+            <x-shared::avatar :src="$currentProfile->avatar_url" alt="avatar" class="h-10 w-10 rounded-full me-2" />
             @endif
-            <div class="ms-1">
-                <i class="material-symbols-outlined text-accent">arrow_drop_down</i>
-            </div>
         </button>
     </div>
 
