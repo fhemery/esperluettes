529bf42051cb52cce706d464d175187a02119fcc
sebraza <srazakarivony@gmail.com>
2025-09-16 11:13:32 +0200

    fix bug largeur login / register

--- app/Domains/Auth/Views/register.blade.php
+++ app/Domains/Auth/Views/register.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout seasonal-background="true">
     <div class="h-full flex flex-col items-center py-16">
-        <div class="surface-secondary w-full sm:max-w-md mt-6 px-6 py-4 shadow-md overflow-hidden sm:rounded-lg">
+        <div class="surface-secondary w-full sm:max-w-xl mt-6 px-6 py-4 shadow-md overflow-hidden sm:rounded-lg">
             <form method="POST" action="{{ route('register') }}">
                 @csrf
 
