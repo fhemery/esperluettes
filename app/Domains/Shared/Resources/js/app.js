51b08832956073b97d1829fa9b4dd2cf32dae9b2
fhemery <f.hemery@hemit.fr>
2025-09-25 22:57:06 +0200

    refactor(shared): extracted date formatting function from comments

--- app/Domains/Shared/Resources/js/app.js
+++ app/Domains/Shared/Resources/js/app.js
@@ -2,9 +2,11 @@ import './bootstrap';
 import Alpine from 'alpinejs';
 import registerTooltip from './tooltip.js';
 import { initQuillEditor } from './editor.js';
+import * as DateUtils from './date-utils.js';
 
 window.Alpine = Alpine;
 window.initQuillEditor = initQuillEditor;
+window.DateUtils = DateUtils;
 
 // Register UI components
 registerTooltip(Alpine);
