794da61fbe5eecb1f01eaac6204710c4f9f09bb7
fhemery <f.hemery@hemit.fr>
2025-10-01 16:11:17 +0200

    feat(story): integrate aptos font

--- vite.config.js
+++ vite.config.js
@@ -21,6 +21,10 @@ export default defineConfig(({ mode }) => {
                               target: appUrl,
                               changeOrigin: true,
                           },
+                          '/fonts': {
+                              target: appUrl,
+                              changeOrigin: true,
+                          },
                       },
                   }
                 : {}),
