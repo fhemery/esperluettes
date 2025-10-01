88b5b3c90ae3638e58f57aaa773211a429737a13
fhemery <f.hemery@hemit.fr>
2025-10-01 17:49:50 +0200

    feat(auth): integrate forgot-password page

--- app/Domains/Auth/Private/Resources/views/pages/forgot-password.blade.php
+++ app/Domains/Auth/Private/Resources/views/pages/forgot-password.blade.php
@@ -1,24 +1,24 @@
-<x-app-layout>
-    <div class="min-h-screen flex flex-col items-center pt-16">
-        <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-            <div class="mb-4 text-sm text-gray-600">
+<x-app-layout seasonal-background="true">
+    <div class="flex flex-col items-center pt-16">
+        <div class="surface-secondary text-on-surface w-full sm:max-w-md mt-6 px-6 py-4 overflow-hidden">
+            <div class="mb-4 text-sm">
                 {{ __('auth::forgot.intro') }}
             </div>
 
             <!-- Session Status -->
             <x-auth-session-status class="mb-4" :status="session('status')" />
 
-            <form method="POST" action="{{ route('password.email') }}">
+            <form method="POST" action="{{ route('password.email') }}" class="flex flex-col gap-4">
                 @csrf
 
                 <!-- Email Address -->
-                <div>
-                    <x-input-label for="email" :value="__('auth::forgot.email')" />
-                    <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-                    <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                <div class="flex flex-col gap-2">
+                    <x-input-label for="email" class="text-on-surface" :value="__('auth::forgot.email')" />
+                    <x-text-input id="email" class="w-full text-fg" type="email" name="email" :value="old('email')" required autofocus />
+                    <x-input-error :messages="$errors->get('email')" class="error-on-surface" />
                 </div>
 
-                <div class="flex items-center justify-end mt-4">
+                <div class="flex items-center justify-end">
                     <x-shared::button color="accent" type="submit">
                         {{ __('auth::forgot.submit') }}
                     </x-shared::button>
--- app/Domains/Shared/Resources/views/components/text-input.blade.php
+++ app/Domains/Shared/Resources/views/components/text-input.blade.php
@@ -1,3 +1,3 @@
-@props(['disabled' => false])
+@props(['disabled' => false, 'color' => 'accent'])
 
-<input @disabled($disabled) {{ $attributes->merge(['class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm']) }}>
+<input @disabled($disabled) {{ $attributes->merge(['class' => 'border-accent focus:border-accent/80 focus:ring-accent rounded-md']) }}>
