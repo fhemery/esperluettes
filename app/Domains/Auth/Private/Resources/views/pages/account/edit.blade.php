f7c07f69a50074ac5c3ba3f6ecbfd9ca46ce0bb0
fhemery <f.hemery@hemit.fr>
2025-10-06 15:54:26 +0200

    fix(auth): fix shadows and rounded corners on account page

--- app/Domains/Auth/Private/Resources/views/pages/account/edit.blade.php
+++ app/Domains/Auth/Private/Resources/views/pages/account/edit.blade.php
@@ -4,11 +4,11 @@
             @include('auth::account.partials.update-account-information-form')
         </div>
 
-        <div class="my-2 p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+        <div class="my-2 p-4 sm:p-8 surface-read text-on-surface">
             @include('auth::account.partials.update-password-form')
         </div>
 
-        <div class="my-2 p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+        <div class="my-2 p-4 sm:p-8 surface-read text-on-surface">
             @include('auth::account.partials.delete-user-form')
         </div>
     </div>
