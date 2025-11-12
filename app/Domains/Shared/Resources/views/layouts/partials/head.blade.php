2141c7a24f111221e6dabcb930612b1f0d77fa66
fhemery <f.hemery@hemit.fr>
2025-11-12 16:24:31 +0100

    fix(meta): tentative fix of icons for search engines

--- app/Domains/Shared/Resources/views/layouts/partials/head.blade.php
+++ app/Domains/Shared/Resources/views/layouts/partials/head.blade.php
@@ -4,6 +4,14 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 @endif
 
+<!-- Favicons -->
+<link rel="icon" href="{{ asset('favicon.ico') }}" sizes="any">
+<link rel="icon" type="image/png" sizes="48x48" href="{{ asset('favicon-48x48.png') }}">
+<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}">
+<link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
+<link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicon-180x180.png') }}">
+<meta name="theme-color" content="#f7eadf">
+
 <!-- Fonts -->
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
