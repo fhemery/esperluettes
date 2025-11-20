0d9f41b09ffd4e9477306a6f5dcd5f3d16ca2e35
fhemery <f.hemery@hemit.fr>
2025-11-20 15:13:21 +0100

    chore(admin): update the dashboard text

--- app/Domains/Administration/Private/Resources/lang/fr/dashboard.php
+++ app/Domains/Administration/Private/Resources/lang/fr/dashboard.php
@@ -4,19 +4,5 @@ return [
     'title' => 'Tableau de bord',
     'welcome' => 'Bienvenue dans le panneau d\'administration',
     'description' => 'Cette interface vous permet de gérer les aspects techniques et de modération de la plateforme.',
-    'maintenance' => [
-        'title' => 'Maintenance système',
-        'description' => 'Accéder aux outils de maintenance système et de gestion du cache.',
-        'link' => 'Maintenance',
-    ],
-    'logs' => [
-        'title' => 'Journaux système',
-        'description' => 'Consulter les journaux d\'erreur et les logs de l\'application.',
-        'link' => 'Voir les logs',
-    ],
-    'user_management' => [
-        'title' => 'Gestion des utilisateurs',
-        'description' => 'Gérer les comptes utilisateurs et les permissions.',
-        'link' => 'Utilisateurs',
-    ],
+    'temporary_v2' => 'Cette administration cohabite avec l\'ancienne pour des raisons techniques. Pas de panique ! Les menus sont tous présents et utilisent l\'ancien ou le nouveau système suivant l\'état de migration de l\'écran auquel vous voulez accéder.'
 ];
--- app/Domains/Administration/Private/Resources/views/pages/dashboard.blade.php
+++ app/Domains/Administration/Private/Resources/views/pages/dashboard.blade.php
@@ -2,9 +2,13 @@
     <div class="flex flex-col gap-8 surface-read text-on-surface p-4">
         <x-shared::title>{{ __('administration::dashboard.title') }}</x-shared::title>
         
-        <div class="surface-bg text-on-surface p-6">
+        <div class="flex flex-col gap-4 surface-bg text-on-surface p-6">
             <p class="text-lg">{{ __('administration::dashboard.welcome') }}</p>
-            <p class="mt-4">{{ __('administration::dashboard.description') }}</p>
+            <p class="">{{ __('administration::dashboard.description') }}</p>
+            <p class="flex items-center gap-2">
+                <span class="material-symbols-outlined text-warning">info</span>
+                {{ __('administration::dashboard.temporary_v2') }}
+            </p>
         </div>
     </div>
 </x-admin::layout>
