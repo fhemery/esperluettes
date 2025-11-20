cb5185d72846f45dc0baa10ed4c39b563794bec7
fhemery <f.hemery@hemit.fr>
2025-11-20 20:22:11 +0100

    fix(auth): fix small translation issue when account blocked

--- app/Domains/Auth/Private/Resources/lang/fr/login.php
+++ app/Domains/Auth/Private/Resources/lang/fr/login.php
@@ -9,4 +9,11 @@ return [
     'submit' => 'Se connecter',
     'no_account' => 'Pas encore de compte ?',
     'create_one' => 'Créer un compte',
+
+    'error' => [
+         "account_deactivated" => [
+            "message" => "Votre compte a été désactivé par un administrateur. Vous ne pouvez plus vous connecter à l'application.",
+            "contact_info" => "Veuillez vérifier vos e-mails ou contacter directement les administrateurs pour plus d'informations.",
+        ],
+    ],
 ];
