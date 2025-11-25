f7336523a13262cfb7e8acf1491c5ba1238c2d93
sebraza <srazakarivony@gmail.com>
2025-11-25 22:49:28 +0100

    feat(version): change to answer PR

--- app/Domains/Shared/Resources/views/components/footer.blade.php
+++ app/Domains/Shared/Resources/views/components/footer.blade.php
@@ -34,9 +34,11 @@
         </div>
 
         <div class="w-full flex items-center justify-center text-xs text-fg/70 mt-4">
-            <p>&copy; {{ date('Y') }} {{ config('app.name') }}</p>
             <p>
-                Version {{ getAppVersion() }}
+                &copy; {{ date('Y') }} {{ config('app.name') }}
+                @if ($version = \App\Domains\Shared\Helpers\VersionHelper::get())
+                    | Version {{ $version }}
+                @endif
             </p>
         </div>
     </div>
